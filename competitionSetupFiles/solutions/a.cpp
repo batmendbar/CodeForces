e2ac7d19e82487bc713c34117429303be60e3994
Batmend Batsaikhan <67634086+batmendbar@users.noreply.github.com>
2022-04-09 15:23:03 -0500

    Removed unnecessary lines.

--- competitionSetupFiles/solutions/a.cpp
+++ competitionSetupFiles/solutions/a.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//misc
 #define nl '\n';
 typedef long long ll;
 const long long MOD = 1e9 + 7;
@@ -11,13 +10,11 @@ const long long LLINF = (long long) (9e18) + 5;
 const int dx[4] = {1,0,-1,0};
 const int dy[4] = {0,1,0,-1};
 
-// vector
 typedef vector<int> vi;
 typedef vector<long long> vll;
 typedef vector<pair<int, int> > vp;
 #define pb push_back
 
-// pair
 typedef pair<int, int> pii;
 #define mp make_pair
 #define F first
@@ -43,12 +40,10 @@ ll fpow(ll base,ll power){
     ll ans=1;
     while(power>0){
         if(power%2==1){
-            ans *= base;
-            ans %= MOD;
+            ans *= base;            ans %= MOD;
         }
         power/=2;
-        base *= base;
-        base %= MOD;
+        base *= base;        base %= MOD;
     }
     return ans;
 }
@@ -56,20 +51,16 @@ ll fpow(ll base,ll power){
 ll fact(ll n) {
     ll ans = 1;
     for (ll i = 1; i <= n; i++) {
-        ans *= i;
-        ans %= MOD;
+        ans *= i;        ans %= MOD;
     }
     return ans;
 }
  
 ll C(ll n, ll k) {
     ll ans = 1;
-    ans *= fact(n);
-    ans %= MOD;
-    ans *= fpow(fact(k), MOD-2);
-    ans %= MOD;
-    ans *= fpow(fact(n-k), MOD-2);
-    ans %= MOD;
+    ans *= fact(n);    ans %= MOD;
+    ans *= fpow(fact(k), MOD-2);    ans %= MOD;
+    ans *= fpow(fact(n-k), MOD-2);    ans %= MOD;
     return ans;
 }
 
@@ -86,22 +77,14 @@ void sieve(vector<int>& primes, int nn) { // returns vector
 
 
 const int N = (1e5)+5;
+
 //------------------------------------------------------
-
-
-
 
 void solve() {
     cout << "Hello, world!" << nl;
 }
 
-
-
-
 //------------------------------------------------------
-
-
-
 
 int main(int argc, char *argv[]) {
     if (argc > 1) freopen(argv[1], "r", stdin); 
