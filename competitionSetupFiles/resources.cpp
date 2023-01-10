43e405a3b63346e34618962da3b64f8e08f6e59e
Batmend Batsaikhan <batmendbar@gmail.com>
2023-01-10 04:54:05 -0600

    union find and modular int

--- competitionSetupFiles/resources.cpp
+++ competitionSetupFiles/resources.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//TODO: Add factorial and inverse factorials
+
 using ll =  long long;
 // const long long MOD = 1e9 + 7;
 const long long MOD = 998244353;
@@ -24,12 +26,85 @@ ll C(ll n, ll k);
 ll INV(ll n);
 vll primes(int nn);
 
+//Copied from Turkhuu: https://codeforces.com/contest/1783/submission/188470532
+
+template <typename T>
+T power(T a, long long b) {
+  T s = 1;
+  for (; b > 0; a *= a, b /= 2) {
+    if (b % 2 == 1) {
+      s *= a;
+    }
+  }
+  return s;
+}
+
+template <int mod>
+struct modular_int {
+  using mint = modular_int;
+  int value;
+  modular_int() : value(0) {
+  }
+  modular_int(long long v) {
+    if (-mod < v && v < mod) {
+      value = v;
+    } else {
+      value = v % mod;
+    }
+    if (value < 0) {
+      value += mod;
+    }
+  }
+  mint& operator+=(const mint& a) {
+    value += a.value;
+    if (value >= mod) {
+      value -= mod;
+    }
+    return *this;
+  }
+  mint& operator-=(const mint& a) {
+    value -= a.value;
+    if (value < 0) {
+      value += mod;
+    }
+    return *this;
+  }
+  mint& operator*=(const mint& a) {
+    value = (int)((long long)value * a.value % mod);
+    return *this;
+  }
+  mint& operator/=(const mint& a) {
+    return *this *= mint(a).inv();
+  }
+  mint operator-() const {
+    return -value;
+  }
+  mint inv() {
+    return power(*this, mod - 2);
+  }
+  friend mint operator+(const mint& a, const mint& b) {
+    return mint(a) += b;
+  }
+  friend mint operator-(const mint& a, const mint& b) {
+    return mint(a) -= b;
+  }
+  friend mint operator*(const mint& a, const mint& b) {
+    return mint(a) *= b;
+  }
+  friend mint operator/(const mint& a, const mint& b) {
+    return mint(a) /= b;
+  }
+  friend istream& operator>>(istream& is, mint& a) {
+    return is >> a.value;
+  }
+  friend ostream& operator<<(ostream& os, const mint& a) {
+    return os << a.value;
+  }
+};
+using mint = modular_int<998244353>;
 
 void solve() {
-    ll n;
-    cin >> n;
-    vll v(n);
-    read(v, n);
+    
 }
 
 //------------------------------------------------------
