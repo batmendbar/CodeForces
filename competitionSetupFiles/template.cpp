da5c746949424e895678181fbe3be8b87d4283fb
Batmend Batsaikhan <batmendbar@gmail.com>
2023-01-23 23:12:04 -0600

    Lil changes

--- competitionSetupFiles/template.cpp
+++ competitionSetupFiles/template.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 using ll = long long;
 // const long long MOD = 1e9 + 7;
-const long long MOD = 998244353;
+// const long long MOD = 998244353;
 const int IINF = 2e9 + 5; 
 const long long LLINF = (long long)(9e18) + 5;
 
@@ -11,7 +11,7 @@ using vll = vector<long long>;
 using vvll = vector<vector<long long>>;
 using pll = pair<long long, long long>;
 
-void solve() {
+void solve(int I) {
     int n; cin >> n;
     vll v(n);
     for (int i = 0; i < n; i++) {
@@ -23,12 +23,12 @@ void solve() {
 
 int main(int argc, char *argv[])
 {
-    cout.precision(17);
+    cout.precision(7);
     if (argc > 1)
         freopen(argv[1], "r", stdin);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int T; cin >> T; for (int i = 1; i <= T; i++) {solve();}
+    int T; cin >> T; for (int I = 1; I <= T; I++) {solve(I);}
     //solve();
     return 0;
 }
